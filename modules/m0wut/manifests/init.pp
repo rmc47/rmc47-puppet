6b015b119081318f705da090c26ae6dad4ad193a
Robert Chipperfield <robert@syxis.co.uk>
2019-08-16 22:50:44 +0100

    Download and unzip Wordpress

--- modules/m0wut/manifests/init.pp
+++ modules/m0wut/manifests/init.pp
@@ -10,4 +10,18 @@ class m0wut {
     ]:
     ensure => present,
   }
+
+  file { '/var/www/wordpress.tar.gz':
+    ensure => present,
+    source => 'https://en-gb.wordpress.org/latest-en_GB.tar.gz',
+    require => Package['apache2'],
+    source_permissions => ignore,
+  }
+
+  exec { 'Unzip Wordpress':
+    cmd => '/bin/tar xzf /var/www/wordpress.tar.gz --strip-components=1',
+    cwd => '/var/www/html',
+    creates => '/var/www/html/index.php',
+    require => File['/var/www/wordpress.tar.gz'],
+  }
 }
