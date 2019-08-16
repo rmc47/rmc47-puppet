8d8953efdabc5b5daad1f7fe2c2618b7f00a3b32
Robert Chipperfield <robert@syxis.co.uk>
2019-08-16 23:19:51 +0100

    Create and configure WP database and config files

--- modules/m0wut/manifests/init.pp
+++ modules/m0wut/manifests/init.pp
@@ -24,4 +24,30 @@ class m0wut {
     creates => '/var/www/html/index.php',
     require => File['/var/www/wordpress.tar.gz'],
   }
+
+  exec { 'Create WP database':
+    command => '/usr/bin/mysql -e "CREATE DATABASE m0wut_wordpress;"',
+    unless => '/usr/bin/mysql -e "SHOW DATABASES" | grep m0wut_wordpress',
+    require => Package['mariadb-server'],
+  }
+
+  exec { 'Create WP database user':
+    command => '/usr/bin/mysql -e "CREATE USER \'www-data\'@localhost IDENTIFIED VIA unix_socket; GRANT ALL ON m0wut_wordpress.* TO \'www-data\'@localhost; FLUSH PRIVILEGES;',
+    unless => '/usr/bin/mysql -e "SELECT * FROM mysql.user" | grep www-data',
+    require => Exec['Create WP database'],
+  }
+  
+  # Grab ourselves some unique salts
+  file { '/var/www/html/wp-salts.php':
+    ensure => present,
+    source => 'https://api.wordpress.org/secret-key/1.1/salt/',
+    require => Package['apache2'],
+    source_permissions => ignore,
+  }
+
+  file { '/var/www/html/wp-config.php':
+    ensure => present,
+    content => template('m0wut/wp-config.php.erb'),
+    require => Package['apache2'],
+  }
 }
