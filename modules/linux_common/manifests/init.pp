a0db2b641f755091d709abf0757ae9a9b7a82aa3
Robert Chipperfield <robert@syxis.co.uk>
2020-02-24 22:34:15 +0000

    Add standard wheel group

--- modules/linux_common/manifests/init.pp
+++ modules/linux_common/manifests/init.pp
@@ -38,4 +38,14 @@ class linux_common {
     type => 'ssh-rsa',
     key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==',
   }
+
+  group { 'group_wheel':
+    name => "wheel",
+    ensure => present,
+  }
+
+  sudo::conf {'Wheel local admins':
+    priority => 9,
+    content => '%wheel ALL=(ALL) NOPASSWD:ALL'
+  }
 }
