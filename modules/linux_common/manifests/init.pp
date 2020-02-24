631df9fae728eeef72457bf96ed0b555041c728e
Robert Chipperfield <robert@syxis.co.uk>
2020-02-24 22:25:40 +0000

    Tabs to spaces 😢

--- modules/linux_common/manifests/init.pp
+++ modules/linux_common/manifests/init.pp
@@ -1,34 +1,34 @@
 class linux_common {
-	package {'vim':
-		ensure => latest,
-	}
+  package {'vim':
+    ensure => latest,
+  }
 
-	package { 'ntp':
-		ensure => latest,
-	}
+  package { 'ntp':
+    ensure => latest,
+  }
 
-	package { 'git':
-		ensure => latest,
-	}
+  package { 'git':
+    ensure => latest,
+  }
 
-	service {'ntp':
-		ensure => running,
-		require => Package['ntp'],
-	}
+  service {'ntp':
+    ensure => running,
+    require => Package['ntp'],
+  }
 
-	service { 'ssh':
-		enable => true,
-		ensure => running,
-	}
-	
-	file { '/etc/timezone':
-	    ensure => present,
-	    content => "Europe/London\n",
-	}
-	file { '/etc/localtime':
-		ensure => "link",
-		target => "/usr/share/zoneinfo/Europe/London",
-	}
+  service { 'ssh':
+    enable => true,
+    ensure => running,
+  }
+  
+  file { '/etc/timezone':
+      ensure => present,
+      content => "Europe/London\n",
+  }
+  file { '/etc/localtime':
+    ensure => "link",
+    target => "/usr/share/zoneinfo/Europe/London",
+  }
 
   sshkey { 'bitbucket.org':
     type => 'ssh-rsa',
